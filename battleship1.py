0e32a964f77286dc28594380ae0c2224a4d297a2
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-05-01 18:12:11 -0400

    makes player 2 grid invisible

--- battleship1.py
+++ battleship1.py
@@ -4,28 +4,29 @@ from engine import Player, Game
 # setting up pygame
 import pygame
 pygame.init()
+pygame.mouse.set_cursor(pygame.cursors.diamond)
 pygame.font.init()
-pygame.display.set_caption("Battleship")
+pygame.display.set_caption("Single Player Battleship")
 game_font = pygame.font.SysFont('fresansttf', 100)
 
 # global variables
-SQ_SIZE = 45
+SQ_SIZE = 35
 H_MARGIN = SQ_SIZE * 4
 V_MARGIN = SQ_SIZE
 WIDTH = SQ_SIZE * 10 * 2 + H_MARGIN
 HEIGHT = SQ_SIZE * 10 * 2 + V_MARGIN
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 INDENT = 10
-HUMAN1 = False   # If switch to false then it will be two computer playing
+HUMAN1 = True   # If switch to false then it will be two computer playing
 HUMAN2 = False
 
 # colors
-GREY = (40, 50, 60)
-WHITE = (255, 250, 250)
-GREEN = (50, 250, 200)
-BLUE = (50, 150, 200)
-RED = (250, 50, 100)
-ORANGE = (250, 140, 20)
+GREY = (160, 160, 160)
+WHITE = (0, 0, 153)
+GREEN = (204, 153, 255)
+BLUE = (255, 204, 229)
+RED = (153, 0, 0)
+ORANGE = (255, 153, 51)
 COLORS = {"U": GREY, "M": BLUE, "H": ORANGE, "S": RED}
 
 # function to draw a grid
@@ -35,6 +36,17 @@ def draw_grid(player, left=0, top=0, search=False):
         y = top + i // 10 * SQ_SIZE
         square = pygame.Rect(x, y, SQ_SIZE, SQ_SIZE)
         pygame.draw.rect(SCREEN, WHITE, square, width = 3)
+        if search:
+            x += SQ_SIZE // 2
+            y += SQ_SIZE // 2
+            pygame.draw.circle(SCREEN, COLORS[player.search[i]], (x, y), radius=SQ_SIZE//4)
+
+def draw_ai_grid(player, left=0, top=0, search=False):
+    for i in range(100):
+        x = left + i % 10 * SQ_SIZE
+        y = top + i // 10 * SQ_SIZE
+        square = pygame.Rect(x, y, SQ_SIZE, SQ_SIZE)
+        pygame.draw.rect(SCREEN, GREY, square, width = 3)
         if search:
             x += SQ_SIZE // 2
             y += SQ_SIZE // 2
@@ -53,6 +65,19 @@ def draw_ships(player, left=0, top=0):
             height = ship.size * SQ_SIZE - 2*INDENT
         rectangle = pygame.Rect(x, y, width, height)
         pygame.draw.rect(SCREEN, GREEN, rectangle, border_radius=10)
+
+def draw_ai_ships(player, left=0, top=0):
+    for ship in player.ships:
+        x = left + ship.col * SQ_SIZE + INDENT
+        y = top + ship.row * SQ_SIZE + INDENT
+        if ship.orientation == 'h':
+            width = ship.size * SQ_SIZE - 2*INDENT
+            height = SQ_SIZE - 2*INDENT
+        else:
+            width = SQ_SIZE - 2*INDENT
+            height = ship.size * SQ_SIZE - 2*INDENT
+        rectangle = pygame.Rect(x, y, width, height)
+        pygame.draw.rect(SCREEN, GREY, rectangle, border_radius=10)
 
 game = Game(HUMAN1, HUMAN2)
 
@@ -109,11 +134,11 @@ while animating:
 
         # draw position grids
         draw_grid(game.player1, top=(HEIGHT-V_MARGIN)//2 + V_MARGIN)
-        draw_grid(game.player2, left=(WIDTH-H_MARGIN)//2 + H_MARGIN)
+        draw_ai_grid(game.player2, left=(WIDTH-H_MARGIN)//2 + H_MARGIN)
 
         # draw ships onto position grids
         draw_ships(game.player1, top=(HEIGHT-V_MARGIN)//2 + V_MARGIN)
-        draw_ships(game.player2, left=(WIDTH-H_MARGIN)//2 + H_MARGIN)
+        draw_ai_ships(game.player2, left=(WIDTH-H_MARGIN)//2 + H_MARGIN)
 
         # computer moves
         if not game.over and game.computer_turn:
